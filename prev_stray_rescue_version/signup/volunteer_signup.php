01435283c6900d4c998eae356634d766b054b552
farhanadib577@gmail.com <farhanadib577@gmail.com>
2026-05-07 21:56:33 +0600

    commit5678

--- prev_stray_rescue_version/signup/volunteer_signup.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    INCLUDE_ONCE __DIR__."/../template/signup_template.php";
-    signup_template("volunteers" ,$_POST ,$_SERVER);
-?>
--- prev_stray_rescue_version/work_step.md
+++
@@ -1,5 +0,0 @@
-### needed configuration
-
-- ##### for mail
-    in php.ini un comment the openssl option
-    
